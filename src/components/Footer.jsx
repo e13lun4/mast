4a2e938d877a7cab19f156fdae9098fad270ea63
Max Dudnik <e13lun4@yandex.ru>
2024-11-26 19:24:26 +0300

    upd footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,64 @@
-import React from "react"
+import React, { useState } from "react"
+import { FaTelegramPlane } from "react-icons/fa"
 
 const Footer = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false)
+
+  const teamMembers = [
+    { name: "Белан Сергей", link: "https://t.me/Cepbiu_60" },
+    { name: "Дудник Максим", link: "https://t.me/e13lun4" },
+    { name: "Котельников Тимофей", link: "https://t.me/azzardd1" },
+    { name: "Ларин Артем", link: "https://t.me/temaLar1n" }
+  ]
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible)
+  }
+
   return (
     <footer
       id="contact"
       className="bg-dark py-6 px-6 sm:px-12 md:px-24 lg:px-48 flex flex-col md:flex-row justify-between items-center"
     >
-      <div className="text-center md:text-left mb-4 md:mb-0">
+      <div className="text-center md:text-left mb-4 md:mb-0 ">
         <p>© 2024 GitOps. Все права защищены.</p>
       </div>
-      <div className="text-center md:text-right">
-        <p>
+      <div className="text-center md:text-right flex items-center relative">
+        <p className="inline-flex items-center">
           Разработали сайт: Белан Сергей, Дудник Максим, Котельников Тимофей,
           Ларин Артем
         </p>
+        <div className="relative">
+          <button
+            onClick={toggleDropdown}
+            className="ml-2 text-blue-500 hover:text-blue-700 focus:outline-none relative"
+          >
+            <FaTelegramPlane size={24} />
+          </button>
+          {isDropdownVisible && (
+            <ul
+              className={`absolute z-50 right-0 bottom-full mb-2 bg-slate-800 text-white p-4 rounded shadow-lg transition-transform duration-300 ease-in-out transform origin-bottom ${
+                isDropdownVisible
+                  ? "scale-100 opacity-100"
+                  : "scale-0 opacity-0"
+              }`}
+              style={{ minWidth: "150px" }}
+            >
+              {teamMembers.map((member, index) => (
+                <li key={index} className="hover:bg-gray-700 p-2 rounded">
+                  <a
+                    href={member.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-400 hover:underline"
+                  >
+                    {member.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </footer>
   )
